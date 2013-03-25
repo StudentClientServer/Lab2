9d978766fbbb981a9909436734fd588b6246cca9
Artem Kleinschmidt <himmel31@gmail.com>
2013-03-25 21:13:14 +0200

    zxc

--- Client/compile.cmd
+++ Client/compile.cmd
@@ -1,3 +1,2 @@
-rem javac  -sourcepath src -d classes  src\model\*.java src\controller\*.java src\view\*.java
-javac -classpath lib/junit.jar -sourcepath src -d classes src/view/Run.java
+javac -classpath lib/junit.jar -sourcepath src -d classes src/*.java
 pause
